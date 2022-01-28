96bccdbb262d21d64556cf3816e25ad927443576
Mayank Mohan Dixit <61104158+mayank7860@users.noreply.github.com>
2022-01-28 12:26:41 +0530

    Add files via upload

---
+++ App.js
@@ -0,0 +1,14 @@
+import logo from "./logo.svg";
+// import "./App.css";
+import Home from "./Portfolio/Home";
+
+function App() {
+  return (
+    <div>
+      <h1>My Portfolio </h1>
+      <Home />
+    </div>
+  );
+}
+
+export default App;
